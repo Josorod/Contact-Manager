38aad6c02fe3c966822944bd0fa4c0c85b53b6ce
Josorod <romanshemenda@gmail.com>
2021-07-04 17:55:17 +0300

    F

---
+++ ContactManager/SampleData.cs
@@ -0,0 +1,38 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager
+{
+    public class SampleData
+    {
+        public static void Initialize(UserContext context)
+        {
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Users.AddRange(
+                    new User
+                    {
+                        Name = "Roman Shemenda",
+                        BirthDay = new DateTime(2002,11,08),
+                        IsMarried = false,
+                        Phone = "0932308395",
+                        Salary=5000 
+                    },
+                     new User
+                     {
+                         Name = "Antonio Khatri",
+                         BirthDay = new DateTime(1980, 06, 08),
+                         IsMarried = true,
+                         Phone = "9123481972",
+                         Salary = 10000
+                     }                  
+                );
+            context.SaveChanges();
+        }
+    }
+}
